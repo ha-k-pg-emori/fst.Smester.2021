4a8e67b1edbd811cddc22cad6cc8ef89de58dc69
MSI\tenha <tenhak16@gmail.com>
2021-06-29 17:55:18 +0900

    Fighter.cppの中身を実装

---
+++ GameProgramming/C_Plactice10/02_Refactoring/fighter.cpp
@@ -0,0 +1,20 @@
+#include"fighter.h"
+
+Fighter::Fighter()
+{
+	
+	name[0] = {'\0'};
+	 hp = 1;
+	 str = 1;
+	 mag = 1;
+	 tec = 1;
+	 spd = 1;
+	 def = 1;
+	 iuc = 1;
+	 mde = 1;
+}
+Fighter::~Fighter()
+{
+	
+}
+
